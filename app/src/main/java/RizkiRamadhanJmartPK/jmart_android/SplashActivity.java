872d758ed1de00f5241efe931e79efbe25646aa4
Rizki Ramadhan <rizki.ramadhan01@ui.ac.id>
2021-12-19 11:24:41 +0700

    Tutam - Modul 9 - Lab PK

--- app/src/main/java/RizkiRamadhanJmartPK/jmart_android/SplashActivity.java
+++ app/src/main/java/RizkiRamadhanJmartPK/jmart_android/SplashActivity.java
@@ -2,7 +2,9 @@ package RizkiRamadhanJmartPK.jmart_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Class ini berfungsi untuk menampilkan splashscreen
@@ -14,5 +16,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Pass intent to Main Activity
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
