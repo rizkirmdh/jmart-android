b9007db022a6379c877c2aea0829d510e22c9b9c
Rizki Ramadhan <rizki.ramadhan01@ui.ac.id>
2021-12-19 12:11:00 +0700

    Tutam - Modul 9 - Lab PK

--- app/src/main/java/RizkiRamadhanJmartPK/jmart_android/PaymentActivity.java
+++ app/src/main/java/RizkiRamadhanJmartPK/jmart_android/PaymentActivity.java
@@ -35,7 +35,7 @@ public class PaymentActivity extends AppCompatActivity {
         buyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
